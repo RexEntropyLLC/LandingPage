035dee9a43a845bef503d4402702ff215ed02820
AJ Phoenix <ajph03nix@gmail.com>
2023-01-31 22:38:30 -0500

    Toggled hero size

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 let r = document.querySelector(':root')
-let navBtns = document.querySelectorAll('li')
+let navBtns = document.querySelectorAll('.nav-btn')
 let expand = false
 
-function toggleExpand(){
-    if(!expand){expand = true}
-    if(expand){expand = false}
-    console.log('toggleExpand')
+function toggleExpand(e){
+    if(!expand){
+        r.style.setProperty('--header-height', '850px')
+        console.log('Toggle open')
+        expand = true
+    }
+   else  if(expand){
+        r.style.setProperty('--header-height', '600px')
+        console.log('Toggle closed')
+        expand = false
+    }
+    console.log('toggleExpand',e.target.innerHTML)
     
 }
 
-function handleNavCLick(){
-    toggleExpand()
- if(expand){r.style.setProperty('--header-height', '900px')}
+function handleNavCLick(e){
+
+    // console.log('handleNavCLick',e.target.innerHTML)
+    e.preventDefault()
+    toggleExpand(e)
+//  if(expand){r.style.setProperty('--header-height', '900px')}
 //  else{r.style.setProperty('--header-height', '600px')}
 }
 
 
-navBtns.addEventListener('click',handleNavCLick)
+navBtns.forEach(btn => btn.addEventListener('click',e =>{handleNavCLick(e)}))
+// navBtns.addEventListener('click',handleNavCLick)
