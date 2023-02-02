a297103c590d4ad3f27191a4a2f57c9a6821fa55
AJ Phoenix <ajph03nix@gmail.com>
2023-02-01 19:38:12 -0500

    Responsiveness

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
-let r = document.querySelector(':root')
 let navBtns = document.querySelectorAll('.nav-btn')
-let expand = false
+    let contentWrap = document.getElementById("content-wrap")
+    let r = document.querySelector(':root')
 
-function toggleExpand(e){
-    if(!expand){
-        r.style.setProperty('--header-height', '850px')
-        console.log('Toggle open')
-        expand = true
+document.addEventListener("load",()=>{
+
+    navBtns = document.querySelectorAll('.nav-btn')
+    contentWrap = document.getElementById("content-wrap")
+})
+    let expand = false
+    let height = window.innerHeight;
+    function toggleExpand(e){
+        if(!expand){
+            height = window.innerHeight-80;
+            r.style.setProperty('--header-height',` ${height}px`)
+            contentWrap.classList.toggle("show")
+            console.log('Toggle open')
+            expand = true
+        }
+        else  if(expand){
+            r.style.setProperty('--header-height', '600px')
+            contentWrap.classList.toggle("show")
+            console.log('Toggle closed')
+            expand = false
+        }
+        console.log('toggleExpand',e.target.innerHTML)
+        
     }
-   else  if(expand){
-        r.style.setProperty('--header-height', '600px')
-        console.log('Toggle closed')
-        expand = false
-    }
-    console.log('toggleExpand',e.target.innerHTML)
     
-}
+    function handleNavCLick(e){
+    
+        // console.log('handleNavCLick',e.target.innerHTML)
+        e.preventDefault()
+        toggleExpand(e)
+    //  if(expand){r.style.setProperty('--header-height', '900px')}
+    //  else{r.style.setProperty('--header-height', '600px')}
+    }
+    
+    
+    navBtns.forEach(btn => btn.addEventListener('click',e =>{handleNavCLick(e)}))
+    // navBtns.addEventListener('click',handleNavCLick)
 
-function handleNavCLick(e){
-
-    // console.log('handleNavCLick',e.target.innerHTML)
-    e.preventDefault()
-    toggleExpand(e)
-//  if(expand){r.style.setProperty('--header-height', '900px')}
-//  else{r.style.setProperty('--header-height', '600px')}
-}
-
-
-navBtns.forEach(btn => btn.addEventListener('click',e =>{handleNavCLick(e)}))
-// navBtns.addEventListener('click',handleNavCLick)
