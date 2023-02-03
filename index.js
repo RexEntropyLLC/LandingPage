5483ce52f1326ebd76f7dda45a917141cd1835eb
AJ Phoenix <ajph03nix@gmail.com>
2023-02-03 11:41:29 -0500

    Basic layout and interaction

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let navBtns = document.querySelectorAll('.nav-btn')
 let heroLogo = document.getElementById("hero-logo")
+let heroIcon= document.getElementById("hero-icon")
+let modalCloseBtn = document.getElementById("modal-close-btn")
 let contentWrap = document.querySelector(".modal-fx")
 let r = document.querySelector(':root')
 
@@ -7,38 +9,51 @@ document.addEventListener("load",()=>{
     navBtns = document.querySelectorAll('.nav-btn')
     contentWrap = document.getElementById("content-wrap")
 })
-    let expand = false
-    let height = window.innerHeight;
-    function toggleExpand(e){
-        if(!expand){
-            height = window.innerHeight-80;
-            r.style.setProperty('--header-height',` ${height}px`)
-            contentWrap.classList.toggle("show")
-            heroLogo.classList.toggle("hero-logo-shink")
-            console.log('Toggle open')
-            expand = true
-        }
-        else  if(expand){
-            r.style.setProperty('--header-height', '600px')
-            contentWrap.classList.toggle("show")
-            heroLogo.classList.toggle("hero-logo-shink")
-            console.log('Toggle closed')
-            expand = false 
-        }
-        console.log('toggleExpand',e.target.innerHTML)
+
+let expand = false
+let height = window.innerHeight;
+
+function toggleExpand(e){
+    if(!expand){
+        height = window.innerHeight-80;
+        r.style.setProperty('--header-height',` ${height}px`)
+        heroIcon.classList.toggle("hero-icon-move")
+        contentWrap.classList.toggle("show")
+        // modalCloseBtn.classList.toggle("show")
+        heroLogo.classList.toggle("hero-logo-shink")
+        console.log('Toggle open')
+        expand = true
+    }
+    else  if(expand){
+        r.style.setProperty('--header-height', '600px')
+        heroIcon.classList.toggle("hero-icon-move")
+        contentWrap.classList.toggle("show")
+        // modalCloseBtn.classList.toggle("show")
+        heroLogo.classList.toggle("hero-logo-shink")
+        console.log('Toggle closed')
+        expand = false 
+    }
+    console.log('toggleExpand',e.target.innerHTML) 
+}
+function fillFrame(tag){
+    // document.getElementById('contentFrame').src = './assets/loading-bar.gif';
+    document.getElementById('content-header').innerHTML =`<h2>${tag}</h2>`;
+}
+function handleNavCLick(e){
+    // console.log('handleNavCLick',e.target.innerHTML)
+    let tag = e.target.innerHTML
+    e.preventDefault()
+    if(!expand){
+        toggleExpand(e)
         
     }
+    fillFrame(tag)
+//  if(expand){r.style.setProperty('--header-height', '900px')}
+//  else{r.style.setProperty('--header-height', '600px')}
+}
+
     
-    function handleNavCLick(e){
-    
-        // console.log('handleNavCLick',e.target.innerHTML)
-        e.preventDefault()
-        toggleExpand(e)
-    //  if(expand){r.style.setProperty('--header-height', '900px')}
-    //  else{r.style.setProperty('--header-height', '600px')}
-    }
-    
-    
-    navBtns.forEach(btn => btn.addEventListener('click',e =>{handleNavCLick(e)}))
+navBtns.forEach(btn => btn.addEventListener('click',e =>{handleNavCLick(e)}))
+modalCloseBtn.addEventListener('click',e =>{toggleExpand(e)})
     // navBtns.addEventListener('click',handleNavCLick)
 
